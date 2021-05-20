0d4a187807f4626eee97a660fb7c65f9a8f99e0b
unknown <ankit.singhania767@gmail.com>
2021-05-20 23:18:47 +0530

    added api link

--- src/API/api_call.js
+++ src/API/api_call.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const dotenv = require("dotenv");
 dotenv.config();
 const instance = axios.create({
-  baseURL: "process.env.REACT_APP_BASE_URL",
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 export const callServer = async ({ url, type, body }) => {
   try {
