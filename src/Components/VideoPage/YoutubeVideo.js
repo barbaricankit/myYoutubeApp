c4d948b88a6c5f19bd170c4dffcf5406334cf795
unknown <ankit.singhania767@gmail.com>
2021-05-23 13:33:55 +0530

    updated headercss and history

--- src/Components/VideoPage/YoutubeVideo.js
+++ src/Components/VideoPage/YoutubeVideo.js
@@ -2,13 +2,11 @@ import "../../App.css";
 
 import YouTube from "react-youtube";
 import { usePlaylist } from "../../context/video-context";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/auth-context";
 import { callServer } from "../../API/api_call";
 
 const YoutubeVideo = ({ id, videoId }) => {
   const { dispatch } = usePlaylist();
-  const navigate = useNavigate();
   const {
     authState: { login, userId },
   } = useAuth();
@@ -30,7 +28,7 @@ const YoutubeVideo = ({ id, videoId }) => {
         console.log("Something Went wrong, cannot add the video to history");
       }
     } else {
-      navigate("/signin");
+      dispatch({ type: "ADD_VIDEO_TO_HISTORY", value: id });
     }
   };
   return (
