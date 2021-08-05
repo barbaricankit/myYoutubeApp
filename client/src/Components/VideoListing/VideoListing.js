88a7140513d16f08829ba3862ba1e3af3a22334b
unknown <ankit.singhania767@gmail.com>
2021-08-06 01:13:30 +0530

    added loader

--- client/src/Components/VideoListing/VideoListing.js
+++ client/src/Components/VideoListing/VideoListing.js
@@ -1,57 +1,75 @@
-import { NavLink } from 'react-router-dom';
-import { usePlaylist } from '../../context/video-context';
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import VideoCardDetails from './VideoCardDetails';
-import VideoImage from './VideoImage';
-import { usePlaylistModal } from '../../context/playlist-context';
-import SaveVideo from '../VideoPage/SaveVideo';
-import { useEffect } from 'react';
-import ShowToast from '../AddVideoToPlayList/ShowToast';
+import { NavLink } from 'react-router-dom'
+import { usePlaylist } from '../../context/video-context'
+import { faCircleNotch, faEllipsisV } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import VideoCardDetails from './VideoCardDetails'
+import VideoImage from './VideoImage'
+import { usePlaylistModal } from '../../context/playlist-context'
+import SaveVideo from '../VideoPage/SaveVideo'
+import { useEffect } from 'react'
+import ShowToast from '../AddVideoToPlayList/ShowToast'
 export const VideoListing = () => {
-	const { dispatch, filteredVideos, current_play_video: play_video } = usePlaylist();
-	const { modalState: { playlistModal, video }, modalDispatch } = usePlaylistModal();
-	useEffect(
-		() => {
-			if (play_video) {
-				dispatch({ type: 'REMOVE_PLAY_VIDEO' });
-			}
-			return () => {
-				modalDispatch({ type: 'CLOSE_MODAL' });
-			};
-		},
-		[ dispatch, modalDispatch, play_video ]
-	);
-	return (
-		<div className='content'>
-			<div className='cards'>
-				{filteredVideos.map((video, index) => (
-					<div className='video_link video' key={index}>
-						<NavLink
-							to={`/video/${video.youtube_id}`}
-							className='video_link'
-							onClick={() => {
-								dispatch({ type: 'PLAY_VIDEO', value: video.youtube_id });
-							}}>
-							<div key={video.youtube_id} className='card card-with-text card-text'>
-								<VideoImage video={video} />
+  const {
+    state: { status },
+    dispatch,
+    filteredVideos,
+    current_play_video: play_video,
+  } = usePlaylist()
+  const {
+    modalState: { playlistModal, video },
+    modalDispatch,
+  } = usePlaylistModal()
+  useEffect(() => {
+    if (play_video) {
+      dispatch({ type: 'REMOVE_PLAY_VIDEO' })
+    }
+    return () => {
+      modalDispatch({ type: 'CLOSE_MODAL' })
+    }
+  }, [dispatch, modalDispatch, play_video])
+  console.log({ status })
+  return (
+    <div className="content">
+      {status === 'loading' && (
+        <FontAwesomeIcon
+          icon={faCircleNotch}
+          size="4x"
+          className="btn-color fa-spin"
+        />
+      )}
+      <div className="cards">
+        {filteredVideos.map((video, index) => (
+          <div className="video_link video" key={index}>
+            <NavLink
+              to={`/video/${video.youtube_id}`}
+              className="video_link"
+              onClick={() => {
+                dispatch({ type: 'PLAY_VIDEO', value: video.youtube_id })
+              }}
+            >
+              <div
+                key={video.youtube_id}
+                className="card card-with-text card-text"
+              >
+                <VideoImage video={video} />
 
-								<VideoCardDetails video={video} />
-							</div>
-						</NavLink>
-						<div
-							onClick={() => {
-								modalDispatch({ type: 'OPEN_MODAL', video });
-							}}>
-							<button className='ellipsis-btn'>
-								<FontAwesomeIcon icon={faEllipsisV} />
-							</button>
-						</div>
-					</div>
-				))}
-			</div>
-			{playlistModal && <SaveVideo video={video} />}
-			<ShowToast />
-		</div>
-	);
-};
+                <VideoCardDetails video={video} />
+              </div>
+            </NavLink>
+            <div
+              onClick={() => {
+                modalDispatch({ type: 'OPEN_MODAL', video })
+              }}
+            >
+              <button className="ellipsis-btn">
+                <FontAwesomeIcon icon={faEllipsisV} />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      {playlistModal && <SaveVideo video={video} />}
+      <ShowToast />
+    </div>
+  )
+}
--- client/src/context/video-context.js
+++ client/src/context/video-context.js
@@ -1,30 +1,24 @@
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useReducer,
-  useRef,
-} from "react";
-import { callServer } from "../API/api_call";
-import { useAuth } from "./auth-context";
-import { useData } from "./database-context";
-import { managePlaylistReducer } from "./video-reducer";
-const PlayListContext = createContext();
+import { createContext, useContext, useEffect, useReducer, useRef } from 'react'
+import { callServer } from '../API/api_call'
+import { useAuth } from './auth-context'
+import { useData } from './database-context'
+import { managePlaylistReducer } from './video-reducer'
+const PlayListContext = createContext()
 
 export const PlayListProvider = ({ children }) => {
-  const { videoList } = useData();
-  const videos = Object.assign(videoList);
-  const { authState } = useAuth();
+  const { videoList } = useData()
+  const videos = Object.assign(videoList)
+  const { authState } = useAuth()
   useEffect(() => {
-    dispatch({ type: "SET_VIDEOLIST", value: videos });
-  }, [videos]);
+    dispatch({ type: 'SET_VIDEOLIST', value: videos })
+  }, [videos])
   useEffect(() => {
     if (authState?.userId) {
-      (async () => {
+      ;(async () => {
         const { data } = await callServer({
           url: `/${authState?.userId}/user`,
-          type: "GET",
-        });
+          type: 'GET',
+        })
         if (data) {
           const {
             userId,
@@ -33,7 +27,7 @@ export const PlayListProvider = ({ children }) => {
             playlists: playLists,
             watchlater,
             historyVideos,
-          } = data;
+          } = data
           const serverData = {
             userId,
             likedVideos,
@@ -41,12 +35,12 @@ export const PlayListProvider = ({ children }) => {
             playLists,
             watchlater: watchlater?.videoIds,
             historyVideos,
-          };
-          dispatch({ type: "UPDATE_INITIAL_STATE", value: serverData });
+          }
+          dispatch({ type: 'UPDATE_INITIAL_STATE', value: serverData })
         }
-      })();
+      })()
     }
-  }, [authState]);
+  }, [authState])
   const [state, dispatch] = useReducer(managePlaylistReducer, {
     history: [],
     videolist: videos,
@@ -58,60 +52,60 @@ export const PlayListProvider = ({ children }) => {
     showPlayListOptions: false,
     navPlaylistSelection: null,
     showNav: false,
-    searchText: "",
+    searchText: '',
     userInitials: null,
-  });
-  const navbar = useRef(null);
+    status: 'loading',
+  })
+  const navbar = useRef(null)
 
   const searchedVideos = (videos, searchText) => {
     return videos.filter((video) =>
-      video.title.toLowerCase().includes(searchText.toLowerCase())
-    );
-  };
+      video.title.toLowerCase().includes(searchText.toLowerCase()),
+    )
+  }
   const filterCurrentVideo = (videos, video) => {
-    return videos.filter((vid) => video !== vid.youtube_id);
-  };
-  let filteredVideos = searchedVideos(state.videolist, state.searchText);
+    return videos.filter((vid) => video !== vid.youtube_id)
+  }
+  let filteredVideos = searchedVideos(state.videolist, state.searchText)
 
   filteredVideos.forEach((video) => {
-    const findvideo = state.likedVideos.find(
-      (videoId) => video._id === videoId
-    );
+    const findvideo = state.likedVideos.find((videoId) => video._id === videoId)
     if (findvideo) {
-      video.liked = true;
+      video.liked = true
     } else {
-      video.liked = false;
+      video.liked = false
     }
-  });
+  })
   filteredVideos.forEach((video) => {
     const findvideo = state.dislikedVideos.find(
-      (videoId) => video._id === videoId
-    );
+      (videoId) => video._id === videoId,
+    )
     if (findvideo) {
-      video.disliked = true;
+      video.disliked = true
     } else {
-      video.disliked = false;
+      video.disliked = false
     }
-  });
+  })
   filteredVideos.forEach((video) => {
-    const findvideo = state.watchlater.find((videoId) => video._id === videoId);
+    const findvideo = state.watchlater.find((videoId) => video._id === videoId)
     if (findvideo) {
-      video.watchlater = true;
+      video.watchlater = true
     } else {
-      video.watchlater = false;
+      video.watchlater = false
     }
-  });
+  })
   const current_play_video = filteredVideos.find(
-    ({ youtube_id }) => youtube_id === state.playVideo
-  );
-  filteredVideos = filterCurrentVideo(filteredVideos, state.playVideo);
+    ({ youtube_id }) => youtube_id === state.playVideo,
+  )
+  filteredVideos = filterCurrentVideo(filteredVideos, state.playVideo)
 
   return (
     <PlayListContext.Provider
-      value={{ state, dispatch, navbar, filteredVideos, current_play_video }}>
+      value={{ state, dispatch, navbar, filteredVideos, current_play_video }}
+    >
       {children}
     </PlayListContext.Provider>
-  );
-};
+  )
+}
 
-export const usePlaylist = () => useContext(PlayListContext);
+export const usePlaylist = () => useContext(PlayListContext)
